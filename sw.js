0ad17a433b26565d2ab6e5b89eec9d8eb221ff3e
dksbluesky <dksbluesky@gmail.com>
2026-08-20 16:47:58 +0800

    feat: add installable PWA support

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'stock-revenue-shell-v1';
+const APP_SHELL = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
+self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
+self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key.startsWith('stock-revenue-') && key !== CACHE_NAME).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET' || new URL(request.url).origin !== self.location.origin) return;
+  event.respondWith(caches.match(request).then((cached) => cached || fetch(request)));
+});
